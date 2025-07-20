6a445aeb1ede9422427f89dd3424d91cedf6c4b3
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2025-07-20 21:46:43 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Services from "./pages/Services";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import GetInTouch from "./pages/GetInTouch";
-import PrivacyPolicy from "./pages/privacypolicy";
+// import PrivacyPolicy from "./pages/privacypolicy";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -25,7 +25,7 @@ const App = () => (
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/getintouch" element={<GetInTouch />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          {/* <Route path="/privacy-policy" element={<PrivacyPolicy />} /> */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
